77997b6d6ccb56e6202aaf8570236e71fce0698a
zhuyingye <zhutoumeme@gmail.com>
2018-03-04 17:02:07 +0800

    去除测试文件

--- athena/src/main/java/cn/zyy/union/pojo/UserInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package main\java\cn\zyy\union\pojo;
-
-import main\java\cn\zyy\union\mapper.UserInfo;
-
-public interface UserInfoMapper {
-    int insert(UserInfo record);
-
-    int insertSelective(UserInfo record);
-}
--- athena/src/union/pojo/UserInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package union.pojo;
-
-import union.mapper.UserInfo;
-
-public interface UserInfoMapper {
-    int insert(UserInfo record);
-
-    int insertSelective(UserInfo record);
-}
