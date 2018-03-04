0fc79d31680e1248730b404cfb6d5a3393922c56
zhuyingye <zhutoumeme@gmail.com>
2018-03-04 22:51:53 +0800

    mybatis步骤一

---
+++ dao/src/test/java/cn/zyy/union/SqlMapGenerator.java
@@ -0,0 +1,50 @@
+package cn.zyy.union;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class SqlMapGenerator {
+    public static void main(String[] args) {
+        String genConProp = "C:\\workspace\\union\\dao\\src\\main\\resources\\mybatis\\generatorConfig.xml";
+        System.out.println(new File(genConProp));
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(new File(genConProp));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = null;
+        try {
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
