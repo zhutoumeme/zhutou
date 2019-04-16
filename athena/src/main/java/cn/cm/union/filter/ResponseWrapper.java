6a02a60e875f08ffe54424af4ba0db864e77225e
zhutoumeme <zhutoumeme@gmail.com>
2019-04-16 22:59:24 +0800

    HttpResponseServletWrapper

---
+++ athena/src/main/java/cn/cm/union/filter/ResponseWrapper.java
@@ -0,0 +1,27 @@
+package cn.cm.union.filter;
+
+import java.io.CharArrayWriter;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+public class ResponseWrapper extends HttpServletResponseWrapper {
+
+  private CharArrayWriter bufferedWriter = new CharArrayWriter();
+  private PrintWriter cachedWriter = new PrintWriter(bufferedWriter);
+
+  public ResponseWrapper(HttpServletResponse response) {
+    super(response);
+  }
+
+
+  @Override
+  public PrintWriter getWriter() {
+    return cachedWriter;
+
+  }
+
+  public String getResult() {
+    return bufferedWriter.toString();
+  }
+}
